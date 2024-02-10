f9bb3ce6857e000893c0ecadb3a1504c6309a9be
Bigizic <olalekanisaac75@gmail.com>
2024-02-10 22:47:01 +0000

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -2,3 +2,12 @@
  * The file that the server included from.
  */
 
+import AppController from '../controllers/AppController';
+
+const mapRoutes = (app) => {
+  app.get('/status', AppController.getStatus);
+  app.get('/stats', StudentsController.getStats);
+};
+
+export default mapRoutes;
+module.exports = mapRoutes;
