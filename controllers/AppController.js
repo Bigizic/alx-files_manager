fc8e00a0782ec0f357bab1dfecc22ae673f7390a
Bigizic <olalekanisaac75@gmail.com>
2024-02-11 16:09:29 +0000

    Update controllers

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,3 +3,21 @@
  * in the index.js file
  */
 
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AppController {
+  static async getStatus(req, res) {
+    const redisIsAlive = redisClient.isAlive();
+    const dbIsAlive = dbClient.isAlive();
+    res.status(200).json({ redis: redisIsAlive, db: dbIsAlive });
+  }
+
+  static async getStats(req, res) {
+    const nbUsers = await dbClient.nbUsers();
+    const nbFiles = await dbClient.nbFiles();
+    res.status(200).json({ users: nbUsers, files: nbFiles });
+  }
+}
+
+module.exports = AppController;
