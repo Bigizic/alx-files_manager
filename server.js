e8366d632b9da83915439a89a0b4e951d0789ecc
Bigizic <olalekanisaac75@gmail.com>
2024-02-11 23:10:18 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const exp = require('express');
+const routes = require('./routes');
 
 /**
  * Server starter module
@@ -6,12 +7,11 @@ const exp = require('express');
 
 const appss = exp();
 const portss = process.env.PORT || 5000;
-const rout = require('./routes/index');
 
 // eslint-disable-next-line jest/require-hook
 appss.use(exp.json());
 // eslint-disable-next-line jest/require-hook
-appss.use('/', rout);
+appss.use(routes);
 
 // eslint-disable-next-line jest/require-hook
 appss.listen(portss, () => {
