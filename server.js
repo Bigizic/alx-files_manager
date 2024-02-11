07cdccff13d13cf7eb80e43d3a43b2d063a39adf
Bigizic <olalekanisaac75@gmail.com>
2024-02-11 16:13:36 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,19 @@
  */
 
 const exp = require('express');
+
 const appss = exp();
 const portss = process.env.PORT || 5000;
 const rout = require('./routes/index');
 
+// eslint-disable-next-line jest/require-hook
 appss.use(exp.json());
+// eslint-disable-next-line jest/require-hook
 appss.use('/', rout);
 
+// eslint-disable-next-line jest/require-hook
 appss.listen(portss, () => {
-	console.log(`Server running on port ${portss}`);
+  console.log(`Server running on port ${portss}`);
 });
 
 export default appss;
