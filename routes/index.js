1e50841acb40eb9a029a3f7610bf24a95644541d
Bigizic <olalekanisaac75@gmail.com>
2024-02-11 16:07:01 +0000

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 /**
  * The file that the server included from.
  */
-
+const express = require('express');
 import AppController from '../controllers/AppController';
+
+const app = express();
 
 const mapRoutes = (app) => {
   app.get('/status', AppController.getStatus);
-  app.get('/stats', StudentsController.getStats);
+  app.get('/stats', AppController.getStats);
 };
 
 export default mapRoutes;
