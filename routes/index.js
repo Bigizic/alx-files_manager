811f3caa99b747a1b17a26814a7c012c7f636f26
Bigizic <olalekanisaac75@gmail.com>
2024-02-12 13:04:06 +0000

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
-// eslint-disable-next-line no-unused-vars
-import { Express } from 'express';
 import AppController from '../controllers/AppController';
-// import UsersController from '../controllers/UsersController'
+import UsersController from '../controllers/UsersController'
 // import AuthController from  '../controllers/AuthController';
 
 /**
@@ -11,8 +9,8 @@ import AppController from '../controllers/AppController';
 const mapRoutes = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
-  /* app.post('/users', UsersController.postNew);
-  app.get('/connect', AuthController.getConnect);
+  app.post('/users', UsersController.postNew);
+  /*app.get('/connect', AuthController.getConnect);
   app.get('/disconnect', AuthController.getDisconnect);
   app.get('/users/me', UserController.getMe); */
 };
