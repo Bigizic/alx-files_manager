f6227d74a3946bb0f9c4fd1117f38f0d972ed82b
Bigizic <olalekanisaac75@gmail.com>
2024-02-12 15:52:54 +0000

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
+#!/usr/bin/node
+
+// const AppController = require('../controllers/AppController');
+// const UsersController = require('../controllers/UsersController');
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController'
+import UsersController from '../controllers/UsersController';
 // import AuthController from  '../controllers/AuthController';
 
 /**
@@ -10,7 +14,7 @@ const mapRoutes = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
   app.post('/users', UsersController.postNew);
-  /*app.get('/connect', AuthController.getConnect);
+  /* app.get('/connect', AuthController.getConnect);
   app.get('/disconnect', AuthController.getDisconnect);
   app.get('/users/me', UserController.getMe); */
 };
