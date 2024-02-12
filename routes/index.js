27d0fb3bba485a66213e634ed439d63405e58e97
Bigizic <olalekanisaac75@gmail.com>
2024-02-12 12:58:26 +0000

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line no-unused-vars
+import { Express } from 'express';
 import AppController from '../controllers/AppController';
 // import UsersController from '../controllers/UsersController'
 // import AuthController from  '../controllers/AuthController';
