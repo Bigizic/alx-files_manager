5504a10e68d4d41af105a8f331f28d898f5a2357
Bigizic <olalekanisaac75@gmail.com>
2024-02-13 02:27:28 +0000

    Isaac (#1)
    * Update controllers
    
    * Update utils
    
    * Update routes
    
    * Update routes
    
    * Update utils

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
 // const UsersController = require('../controllers/UsersController');
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-// import AuthController from  '../controllers/AuthController';
+import AuthController from '../controllers/AuthController';
 
 /**
  * Contains all endpoints
@@ -14,9 +14,9 @@ const mapRoutes = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
   app.post('/users', UsersController.postNew);
-  /* app.get('/connect', AuthController.getConnect);
+  app.get('/connect', AuthController.getConnect);
   app.get('/disconnect', AuthController.getDisconnect);
-  app.get('/users/me', UserController.getMe); */
+  // app.get('/users/me', UserController.getMe);
 };
 
 export default mapRoutes;
