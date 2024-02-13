63b19676c90b2a7e8af4bbc132bbcbb4b6309f37
Bigizic <olalekanisaac75@gmail.com>
2024-02-13 02:26:46 +0000

    Update utils

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 export const splitAuthHeader = (header) => {
-    const splitter = header.split(' ');
-    if (splitter[0] !== 'Basic') { return null; }
+  const splitter = header.split(' ');
+  if (splitter[0] !== 'Basic') { return null; }
 
-    return splitter[1];
-}
+  return splitter[1];
+};
 
 export const getParams = (string) => {
   const [email, password] = string.split(':');
@@ -13,4 +13,4 @@ export const getParams = (string) => {
     return null;
   }
   return [email, password];
-}
+};
