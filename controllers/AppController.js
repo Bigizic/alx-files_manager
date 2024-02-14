2cb738e792d72fbc62cbff74030b77919ea57dc9
Bigizic <olalekanisaac75@gmail.com>
2024-02-14 22:22:03 +0000

    Update controllers

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,18 +5,26 @@ const redisClient = require('../utils/redis');
 // eslint-disable-next-line no-undef
 const dbClient = require('../utils/db');
 
+// AppController - Controller Module for Application Status plus extra checks
 class AppController {
   /**
-   * AppController - 
+   * getStatus - Get all DB, Connection Status
    * @param {Request object} req
    * @param {HTTP Response} res
-   * @returns HTTP Response.status(Integer).json(Response Body)
+   * @returns {redis: status, MongoDB: status}
    */
   static getStatus(req, res) {
     const redisIsAlive = redisClient.isAlive();
     const dbIsAlive = dbClient.isAlive();
     res.status(200).json({ redis: redisIsAlive, db: dbIsAlive });
   }
+
+  /**
+   * getStats - Get number of documents in a Mongo database
+   * @param {Request object} req
+   * @param {HTTP Response} res
+   * @returns {users: db.collection.users.countDocuments, files: db.collection.files.countDocuments}
+   */
 
   static async getStats(req, res) {
     const nbUsers = await dbClient.nbUsers();
