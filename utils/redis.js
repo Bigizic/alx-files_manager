87214e977fa880ead5c38681ec376cbed58a126b
Bigizic <olalekanisaac75@gmail.com>
2024-02-14 19:48:39 +0000

    Update utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-// const { createClient } = require('redis');
-// const { promisify } = require('util');
-import { createClient } from "redis";
-import { promisify } from 'util';
+const { createClient } = require('redis');
+const { promisify } = require('util');
+// import { createClient } from "redis";
+// import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
