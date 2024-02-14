2dce42150d73de9194c7b3e38a7567035431c1f3
Bigizic <olalekanisaac75@gmail.com>
2024-02-14 21:52:09 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
 
+// eslint-disable-next-line no-undef
 const express = require('express');
+// eslint-disable-next-line no-undef
+const process = require('process');
+// eslint-disable-next-line no-undef
 const mapRoutes = require('./routes');
 
 const appss = express();
 const portss = process.env.PORT || 5000;
 
-// eslint-disable-next-line jest/require-hook
 appss.use(express.json({ limit: '200mb' }));
 
-// eslint-disable-next-line jest/require-hook
 mapRoutes(appss);
 
-// eslint-disable-next-line jest/require-hook
 appss.listen(portss, () => {
   console.log(`Server running on port ${portss}`);
 });
 
-export default appss;
+// export default appss;
